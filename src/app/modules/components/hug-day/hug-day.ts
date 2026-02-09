9aa6e896fc08634a9f6a1c3f9a06854918cd1392
karanveerSsingh <karanveer0508singh@gmail.com>
2026-02-09 22:54:49 +0530

    fixss

--- src/app/modules/components/hug-day/hug-day.ts
+++ src/app/modules/components/hug-day/hug-day.ts
@@ -6,6 +6,4 @@ import { Component } from '@angular/core';
   templateUrl: './hug-day.html',
   styleUrl: './hug-day.scss',
 })
-export class HugDay {
-
-}
+export class HugDay {}
--- src/app/modules/components/promise-day/promise-day.ts
+++ src/app/modules/components/promise-day/promise-day.ts
@@ -13,10 +13,13 @@ export class PromiseDay {
     'I promise to always be with you.',
     'I promise to make you smile, even on the toughest days.',
     'I promise to support your dreams and ambitions.',
-    'I promise to be your best friend, always and forever.',
+    'I promise to be your Boyfriend, always and forever.',
     'I promise to love you unconditionally.',
     'I promise to always listen to you.',
     'I promise to share my fries with you.',
+    'I promise to always be honest with you.',
+    'I promise to make you laugh every day.',
+    'I promise to never stop loving you.',
   ];
 
   onShowPromisesClick() {
