3a3ce33010e8ca0b2c148655dcb5e2dcbee2b1be
karanveerSsingh <karanveer0508singh@gmail.com>
2026-02-14 02:40:43 +0530

    up

--- src/app/modules/components/love-letter/love-letter.ts
+++ src/app/modules/components/love-letter/love-letter.ts
@@ -15,67 +15,15 @@ import {
   styleUrls: ['./love-letter.scss'],
   standalone: true,
 })
-export class LoveLetterComponent implements OnInit, OnDestroy, AfterViewInit {
+export class LoveLetterComponent implements OnInit {
   @Output() celebrate = new EventEmitter<void>();
   @Output() backToPhoto = new EventEmitter<void>();
   @ViewChild('noteContent', { static: false }) noteContent!: ElementRef<HTMLDivElement>;
 
   Array = Array; // Make Array constructor available in template
 
-  private autoScrollInterval: any;
-  private isUserScrolling = false;
-  private userScrollTimeout: any;
-
   ngOnInit() {
     // Component initialization
-  }
-
-  ngAfterViewInit() {
-    this.startAutoScroll();
-  }
-
-  ngOnDestroy() {
-    this.stopAutoScroll();
-    if (this.userScrollTimeout) {
-      clearTimeout(this.userScrollTimeout);
-    }
-  }
-
-  startAutoScroll() {
-    this.autoScrollInterval = setInterval(() => {
-      if (!this.isUserScrolling && this.noteContent) {
-        const element = this.noteContent.nativeElement;
-
-        // Check if we've reached the bottom
-        if (element.scrollTop + element.clientHeight >= element.scrollHeight) {
-          // Reset to top
-          element.scrollTop = 0;
-        } else {
-          // Scroll down slowly
-          element.scrollTop += 1;
-        }
-      }
-    }, 50); // Adjust speed as needed (lower = faster)
-  }
-
-  stopAutoScroll() {
-    if (this.autoScrollInterval) {
-      clearInterval(this.autoScrollInterval);
-    }
-  }
-
-  onUserScroll() {
-    this.isUserScrolling = true;
-
-    // Clear existing timeout
-    if (this.userScrollTimeout) {
-      clearTimeout(this.userScrollTimeout);
-    }
-
-    // Resume auto-scroll after user stops scrolling for 3 seconds
-    this.userScrollTimeout = setTimeout(() => {
-      this.isUserScrolling = false;
-    }, 40);
   }
 
   onCelebrate() {
